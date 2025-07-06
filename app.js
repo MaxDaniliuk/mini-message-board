2e81dfd60d61f7c098053fda0f1da821e9c02096
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2025-07-06 11:51:12 +0300

    Style the index page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ PORT = 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", messagesController.getMessages);
 
