dfd37a19f67423c38dda4bbc2aebbb1a6bed51dc
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2025-07-12 20:11:38 +0300

    Store PORT inside a .env and use the value

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const messagesController = require("./controllers/messagesController");
 const messageRoutes = require("./routes/messageRoutes");
 
 const app = express();
-PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
